635a21bd1d4160a021333899542d18602e608f0c
Radovan Draskovic <73-2021@pmf.kg.ac.rs>
2025-11-22 15:01:14 +0100

    Add ListArtifactsRequest and ListArtifactsResponse classes
    Introduce `ListArtifactsRequest` and `ListArtifactsResponse`
    classes to handle request and response payloads for listing
    artifacts in an MLflow run.
    
    - Added `ListArtifactsRequest` with `RunId`, `Path`, and
      `PageToken` properties, using `JsonProperty` for JSON
      serialization.
    - Added `ListArtifactsResponse` with `RootUri`, `Files`, and
      `NextPageToken` properties, where `Files` is a list of
      `FileInfo` objects.
    - Integrated `Newtonsoft.Json` for JSON serialization in both
      classes.
    - Added dependency on `MLflowClient.Http.Models` for the
      `FileInfo` class.

---
+++ MLflowClient/Http/Dto/Artifacts/Request/ListArtifactsRequest.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace MLflowClient.Http.Dto.Artifacts.Request
+{
+    public class ListArtifactsRequest
+    {
+        [JsonProperty("run_id")]
+        public string RunId { get; set; }
+        [JsonProperty("path")]
+        public string Path { get; set; }
+        [JsonProperty("page_token")]
+        public string PageToken { get; set; }
+    }
+}
---
+++ MLflowClient/Http/Dto/Artifacts/Response/ListArtifactsResponse.cs
@@ -0,0 +1,16 @@
+﻿using MLflowClient.Http.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MLflowClient.Http.Dto.Artifacts.Response
+{
+    public class ListArtifactsResponse
+    {
+        [JsonProperty("root_uri")]
+        public string RootUri { get; set; }
+        [JsonProperty("files")]
+        public List<FileInfo> Files { get; set; } = new List<FileInfo>();
+        [JsonProperty("next_page_token")]
+        public string NextPageToken { get; set; }
+    }
+}
